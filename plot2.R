510cab1e039b27dd32f9f0a006754067603957ba
Alberto Wondracek <alb.csw@gmail.com>
2015-11-08 19:26:03 -0200

    Submission of Course Project 1 - Exploratory Data Analysis

--- plot2.R
+++ plot2.R
@@ -1,22 +1,13 @@
-
 data <- read.csv("household_power_consumption.txt", sep=";")
 aDD <- as.Date(data$Date,  format = "%d/%m/%Y")
 
-
 d1 <- aDD == "2007-02-01"
 d1OK <- aDD[d1]
-length(d1OK)
-d1OK
-length(d1)
 resultDAY1 <- data[d1,]
 
 d2 <- aDD == "2007-02-02"
 d2OK <- aDD[d2]
-length(d2OK)
-# d2OK
-length(d2)
 resultDAY2 <- data[d2,]
-
 
 myData <- rbind(resultDAY1, resultDAY2)
 
